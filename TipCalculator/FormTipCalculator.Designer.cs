ebec25872d2de2be1cc4f414a9e4ddd254f684b2
Lakshmi Balasubramaniam <lrb697@gmail.com>
2022-01-27 16:21:35 -0500

    Add project files.

---
+++ TipCalculator/FormTipCalculator.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace TipCalculator {
+    partial class FormTipCalculator {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBillAmt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtTipRate = new System.Windows.Forms.TextBox();
+            this.txtTipAmt = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bill Amount:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtBillAmt
+            // 
+            this.txtBillAmt.Location = new System.Drawing.Point(222, 71);
+            this.txtBillAmt.Name = "txtBillAmt";
+            this.txtBillAmt.Size = new System.Drawing.Size(150, 31);
+            this.txtBillAmt.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tip Percent:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tip Amount:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(61, 304);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(112, 34);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate Tip";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtTipRate
+            // 
+            this.txtTipRate.Location = new System.Drawing.Point(222, 149);
+            this.txtTipRate.Name = "txtTipRate";
+            this.txtTipRate.ReadOnly = true;
+            this.txtTipRate.Size = new System.Drawing.Size(150, 31);
+            this.txtTipRate.TabIndex = 5;
+            // 
+            // txtTipAmt
+            // 
+            this.txtTipAmt.Location = new System.Drawing.Point(222, 221);
+            this.txtTipAmt.Name = "txtTipAmt";
+            this.txtTipAmt.ReadOnly = true;
+            this.txtTipAmt.Size = new System.Drawing.Size(150, 31);
+            this.txtTipAmt.TabIndex = 6;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(243, 304);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(112, 34);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // FormTipCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 381);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtTipAmt);
+            this.Controls.Add(this.txtTipRate);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBillAmt);
+            this.Controls.Add(this.label1);
+            this.Name = "FormTipCalculator";
+            this.Text = "Tip Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBillAmt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtTipRate;
+        private System.Windows.Forms.TextBox txtTipAmt;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ TipCalculator/FormTipCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TipCalculator {
+    public partial class FormTipCalculator : Form {
+        public FormTipCalculator() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e) {
+            Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e) {
+            var billAmount = Convert.ToDecimal(txtBillAmt.Text);
+
+            var tipPercent = 0.2m;
+            txtTipAmt.Text = tipPercent.ToString("p0");
+
+            var tipAmount = billAmount * tipPercent;
+            txtTipAmt.Text = tipAmount.ToString("c");
+        }
+    }
+}
